6ca2494ea22edb2d4200d7d062b6ead4c649ae6d
Melatxnin <n.gossard@rubika-edu.re>
2026-03-26 11:48:01 +0400

    Création d'un nouveau PowerUp indépendant du GM

--- Assets/Scripts/Bonus.cs
+++ Assets/Scripts/Bonus.cs
@@ -1,16 +1,59 @@
+using System.Collections;
 using UnityEngine;
 
-public class Bonus : MonoBehaviour
+public class Bonus : Entity
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private float aliveTime;
+    [SerializeField] private Renderer renderer;
+
+    private void Start()
     {
+        if (renderer == null)
+        {
+            renderer = gameObject.GetComponent<Renderer>();
+        }
+        renderer.enabled = true;
         
+        StartCoroutine(WaitToDestroy());
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        Move();
+    }
+
+    private IEnumerator WaitToDestroy()
+    {
+        float elapsedTime = 0.0f;
+
+        while (elapsedTime < aliveTime)
+        {
+            elapsedTime += Time.deltaTime;
+            renderer.enabled = true;
+            yield return new WaitForSeconds(elapsedTime);
+            renderer.enabled = true;
+        }
+
+        Destroy();
+    }
+
+    public override void Move()
+    {
+        transform.position += Vector3.back * speedEntity * Time.deltaTime;
+
+        LimitPosition(transform.position);
+    }
+
+    protected override void LimitPosition(Vector3 position)
+    {
+        if (position.z < limitsZ.y)
+        {
+            Destroy();
+        }
+    }
+
+    protected override void Destroy()
+    {
+        base.Destroy();
     }
 }
